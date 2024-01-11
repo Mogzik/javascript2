4155722f2699f8f75f0bb982ed60e49b5894ced2
Mogzik <113260847+Mogzik@users.noreply.github.com>
2024-01-11 17:13:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+function WinOpen_z2()
+{
+window.open("zadanie2_maz.html","okienko_z2","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z3()
+{
+window.open("zadanie3_maz.html","okienko_z3","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z4()
+{
+window.open("zadanie4_syk.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+
+
+
+
+function WinOpen_z2_kod()
+{
+window.open("zadanie2_maz-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z3_kod()
+{
+window.open("zadanie3_maz-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z4_kod()
+{
+window.open("zadanie4_maz-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
